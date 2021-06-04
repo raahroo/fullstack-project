e2771e0c65eea03f89ee0c9f15fd46e7f50f5edf
raissa.hohenester@gmail.com <raissa.hohenester@pentlandfirth.com>
2021-06-04 17:39:46 +0200

    Write files with fs

--- src/files.js
+++ src/files.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
 //reading files
-fs.readFile('./src/docs/blog1.txt', (err, data) => { //async op
+/* fs.readFile('./src/docs/blog1.txt', (err, data) => { //async op
     if (err) {
         console.log(err);
     }
     console.log(data.toString()); // show a buffer - package of data
+}) */
+
+// writing files
+fs.writeFile('./src/docs/blog1.txt', 'Hello Node', () => {
+    console.log('File was written');
+});
+
+fs.writeFile('./src/docs/blog2.txt', 'Hello Node 2', () => {
+    console.log('File was written');
 })
