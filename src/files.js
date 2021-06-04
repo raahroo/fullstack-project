fedce5aa43d87d09446f798c03045ad6c2c71adc
raissa.hohenester@gmail.com <raissa.hohenester@pentlandfirth.com>
2021-06-04 17:51:51 +0200

    Create and delete directories

--- src/files.js
+++ src/files.js
@@ -9,10 +9,27 @@ const fs = require('fs');
 }) */
 
 // writing files
-fs.writeFile('./src/docs/blog1.txt', 'Hello Node', () => {
+/* fs.writeFile('./src/docs/blog1.txt', 'Hello Node', () => {
     console.log('File was written');
-});
+}); */
 
-fs.writeFile('./src/docs/blog2.txt', 'Hello Node 2', () => {
+/* fs.writeFile('./src/docs/blog2.txt', 'Hello Node 2', () => {
     console.log('File was written');
-})
+}) */
+
+//directories
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder created');
+    })
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('folder deleted');
+    })
+}
