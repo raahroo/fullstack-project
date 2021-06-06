cec964e5fe2f3ca5273ea0bf78cc92dca6a84caa
Raissa Hohenester <raissa.developer@gmail.com>
2021-06-06 22:58:16 +0200

    Add routing system

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const server = http.createServer((req, res) => {
     
     //set header content type
     res.setHeader('Content-Type', 'text/html');
+
+    let path = './src/views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
  
     //send an html file
-    fs.readFile('./src/views/index.html', (err, data) =>
+    fs.readFile(path, (err, data) =>
     {
         if (err) {
             console.log(err);
