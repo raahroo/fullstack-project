527c916df17511677dff9fc8f90ac4f23198f684
Raissa Hohenester <raissa.developer@gmail.com>
2021-06-06 22:24:42 +0200

    Add server and listen to requests

--- src/streams.js
+++ src/streams.js
@@ -3,9 +3,12 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./src/docs/blog3.txt', {encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./src/docs/blog4.txt');
 
-readStream.on('data', (chunk) => {
+/* readStream.on('data', (chunk) => {
     console.log('----- NEW CHUNCK ------')
     console.log(chunk);
     writeStream.write('\nNEW CHUNK\n');
     writeStream.write(chunk);
-})
+}) */
+
+//piping 
+readStream.pipe(writeStream);
