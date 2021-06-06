c53f7a84eba0301db42ed6d2b92544f1a9ad5a7f
Raissa Hohenester <raissa.developer@gmail.com>
2021-06-06 22:37:29 +0200

    Return plain text as response

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+    
+    //set header content type
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
